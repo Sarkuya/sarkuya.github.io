542ce28f2314d57d2aeb72e9222f8068967ec8ef
Sarkuya <sarkuya@163.com>
2019-06-10 00:17:41 +0800

    To colored triangle.

--- js/webgl-utils.js
+++ js/webgl-utils.js
@@ -1,213 +1,126 @@
-/*
- * Copyright 2010, Google Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+webglUtils = {};
 
-
-/**
- * @fileoverview This file contains functions every webgl program will need
- * a version of one way or another.
- *
- * Instead of setting up a context manually it is recommended to
- * use. This will check for success or failure. On failure it
- * will attempt to present an approriate message to the user.
- *
- *       gl = WebGLUtils.setupWebGL(canvas);
- *
- * For animated WebGL apps use of setTimeout or setInterval are
- * discouraged. It is recommended you structure your rendering
- * loop like this.
- *
- *       function render() {
- *         window.requestAnimFrame(render, canvas);
- *
- *         // do rendering
- *         ...
- *       }
- *       render();
- *
- * This will call your rendering function up to the refresh rate
- * of your display but will stop rendering if your app is not
- * visible.
- */
-
-WebGLUtils = function() {
-
-/**
- * Creates the HTLM for a failure message
- * @param {string} canvasContainerId id of container of th
- *        canvas.
- * @return {string} The html.
- */
-var makeFailHTML = function(msg) {
-  return '' +
-    '<table style="background-color: #8CE; width: 100%; height: 100%;"><tr>' +
-    '<td align="center">' +
-    '<div style="display: table-cell; vertical-align: middle;">' +
-    '<div style="">' + msg + '</div>' +
-    '</div>' +
-    '</td></tr></table>';
-};
-
-/**
- * Mesasge for getting a webgl browser
- * @type {string}
- */
-var GET_A_WEBGL_BROWSER = '' +
-  'This page requires a browser that supports WebGL.<br/>' +
-  '<a href="http://get.webgl.org">Click here to upgrade your browser.</a>';
-
-/**
- * Mesasge for need better hardware
- * @type {string}
- */
-var OTHER_PROBLEM = '' +
-  "It doesn't appear your computer can support WebGL.<br/>" +
-  '<a href="http://get.webgl.org/troubleshooting/">Click here for more information.</a>';
-
-/**
- * Mesasge for need better hardware
- * @type {string}
- */
-var WEBGL2_COMPUTE_NOT_SUPPORTED = '' +
-  "It doesn't appear your computer can support WebGL 2.0 Compute.<br/>" +
-  '<a href="http://get.webgl.org/troubleshooting/">Click here for more information.</a>';
-
-/**
- * Creates a webgl context. If creation fails it will
- * change the contents of the container of the <canvas>
- * tag to an error message with the correct links for WebGL.
- * @param {Element} canvas. The canvas element to create a
- *     context from.
- * @param {WebGLContextCreationAttirbutes} opt_attribs Any
- *     creation attributes you want to pass in.
- * @return {WebGLRenderingContext} The created context.
- */
-var setupWebGL = function(canvas, opt_attribs) {
-  function showLink(str) {
-    var container = canvas.parentNode;
-    if (container) {
-      container.innerHTML = makeFailHTML(str);
+webglUtils.initContext = function (canvasId) {
+    if (!window.WebGLRenderingContext) {
+        alert("Your browser doese not support WebGL!");
+        return null;
     }
-  };
-
-  if (!window.WebGLRenderingContext) {
-    showLink(GET_A_WEBGL_BROWSER);
-    return null;
-  }
-
-  var context = create3DContext(canvas, opt_attribs);
-  if (!context) {
-    showLink(OTHER_PROBLEM);
-  }
-  return context;
-};
-
-/**
- * Creates a webgl2-compute context. If creation fails it will
- * change the contents of the container of the <canvas>
- * tag to an error message with the correct links for WebGL.
- * @param {Element} canvas. The canvas element to create a
- *     context from.
- * @param {WebGLContextCreationAttirbutes} opt_attribs Any
- *     creation attributes you want to pass in.
- * @return {WebGLRenderingContext} The created context.
- */
-var setupWebGL2Compute = function(canvas, opt_attribs) {
-  function showLink(str) {
-    var container = canvas.parentNode;
-    if (container) {
-      container.innerHTML = makeFailHTML(str);
+    
+    var canvas = document.getElementById(canvasId);
+    if (!canvas) {
+        alert("Unable to find '" + canvasId + "' element");
+        return null;
     }
-  };
 
-  if (!window.WebGLRenderingContext) {
-    showLink(GET_A_WEBGL_BROWSER);
-    return null;
-  }
+    var ctx = canvas.getContext("webgl");
 
-  var context = canvas.getContext("webgl2-compute", opt_attribs);
-  if (!context) {
-    showLink(WEBGL2_COMPUTE_NOT_SUPPORTED);
-  }
-  return context;
-};
-
-/**
- * Creates a webgl context.
- * @param {!Canvas} canvas The canvas tag to get context
- *     from. If one is not passed in one will be created.
- * @return {!WebGLContext} The created context.
- */
-var create3DContext = function(canvas, opt_attribs) {
-  var names = ["webgl", "experimental-webgl"];
-  var context = null;
-  for (var ii = 0; ii < names.length; ++ii) {
-    try {
-      context = canvas.getContext(names[ii], opt_attribs);
-    } catch(e) {}
-    if (context) {
-      break;
+    if (!ctx) {
+        alert("Unable to initialize WebGL!");
+        return null;
     }
-  }
-  return context;
+    
+    return ctx;
 };
 
-return {
-  create3DContext: create3DContext,
-  setupWebGL: setupWebGL,
-  setupWebGL2Compute: setupWebGL2Compute
+webglUtils.prepareProgram = function (ctx, vshader, fshader) {
+    var vertexShader = this.loadShader(ctx, vshader);
+    var fragmentShader = this.loadShader(ctx, fshader);
+
+    var program = ctx.createProgram();
+
+    ctx.attachShader(program, vertexShader);
+    ctx.attachShader(program, fragmentShader);
+
+    ctx.linkProgram(program);
+
+    var linked = ctx.getProgramParameter(program, ctx.LINK_STATUS);
+    if (!linked) {
+        var error = ctx.getProgramInfoLog(program);
+        alert("Error in program linking: " + error);
+
+        ctx.deleteProgram(program);
+        ctx.deleteProgram(fragmentShader);
+        ctx.deleteProgram(vertexShader);
+
+        return null;
+    }
+
+    ctx.useProgram(program);
+
+    return program;
 };
-}();
 
-/**
- * Provides requestAnimationFrame in a cross browser way.
- */
-window.requestAnimFrame = (function() {
-  return window.requestAnimationFrame ||
-         window.webkitRequestAnimationFrame ||
-         window.mozRequestAnimationFrame ||
-         window.oRequestAnimationFrame ||
-         window.msRequestAnimationFrame ||
-         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-           return window.setTimeout(callback, 1000/60);
-         };
-})();
+webglUtils.loadShader = function (ctx, shaderId) {
+    var shaderScript = document.getElementById(shaderId);
 
-/**
- * Provides cancelAnimationFrame in a cross browser way.
- */
-window.cancelAnimFrame = (function() {
-  return window.cancelAnimationFrame ||
-         window.webkitCancelAnimationFrame ||
-         window.mozCancelAnimationFrame ||
-         window.oCancelAnimationFrame ||
-         window.msCancelAnimationFrame ||
-         window.clearTimeout;
-})();
+    if (!shaderScript) {
+        alert("Error: shader script '" + shaderId + "' not found");
+        return null;
+    }
+
+    var shaderType;
+
+    if (shaderScript.type === "x-shader/x-vertex")
+        shaderType = ctx.VERTEX_SHADER;
+    else if (shaderScript.type === "x-shader/x-fragment")
+        shaderType = ctx.FRAGMENT_SHADER;
+    else {
+        alert("Error: shader script '" + shaderId + "' of undefined type '" + shaderScript.type + "'");
+        return null;
+    }
+
+    var shader = ctx.createShader(shaderType);
+
+    ctx.shaderSource(shader, shaderScript.text);
+    ctx.compileShader(shader);
+
+    var compiled = ctx.getShaderParameter(shader, ctx.COMPILE_STATUS);
+    if (!compiled) {
+        var error = ctx.getShaderInfoLog(shader);
+        alert("Error compiling shader '" + shaderId + "':" + error);
+        ctx.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+};
+
+webglUtils.getActiveAttribs = function (ctx) {
+    var maxVertexAttribs = ctx.getParameter(ctx.MAX_VERTEX_ATTRIBS);
+
+    for (var i = 0; i < maxVertexAttribs; i++) {
+        var activeAttrib = ctx.getActiveAttrib(ctx.program, i);
+
+        if (activeAttrib) {
+            console.log(i);
+            console.log(activeAttrib);
+        }
+    }
+};
+
+webglUtils.getVertexAttribs = function (ctx) {
+    var maxVertexAttribs = ctx.getParameter(ctx.MAX_VERTEX_ATTRIBS);
+
+    for (var i = 0; i < maxVertexAttribs; i++) {
+        var flag = ctx.getVertexAttrib(i, ctx.VERTEX_ATTRIB_ARRAY_ENABLED);
+
+        if (flag) {
+            var bufferBinding = ctx.getVertexAttrib(i, ctx.VERTEX_ATTRIB_ARRAY_BUFFER_BINDING);
+            var isEnabled = ctx.getVertexAttrib(i, ctx.VERTEX_ATTRIB_ARRAY_ENABLED);
+            var arraySize = ctx.getVertexAttrib(i, ctx.VERTEX_ATTRIB_ARRAY_SIZE);
+            var arrayStride = ctx.getVertexAttrib(i, ctx.VERTEX_ATTRIB_ARRAY_STRIDE);
+            var arrayType = ctx.getVertexAttrib(i, ctx.VERTEX_ATTRIB_ARRAY_TYPE);
+            var isNormalized = ctx.getVertexAttrib(i, ctx.VERTEX_ATTRIB_ARRAY_NORMALIZED);
+            var currentVertexAttrib = ctx.getVertexAttrib(i, ctx.CURRENT_VERTEX_ATTRIB);
+
+            console.log(i);
+            console.log(bufferBinding);
+            console.log(isEnabled);
+            console.log(arraySize);
+            console.log(arrayStride);
+            console.log(arrayType);
+            console.log(isNormalized);
+            console.log(currentVertexAttrib);
+        }
+    }
+};
