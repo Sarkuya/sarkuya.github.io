7112ce46df45938cf9da1ad6898e6f7119c820e2
sarkuya@163.com <>
2020-05-18 02:06:46 +0800

    加入观梅占案例分析。

--- apps/mhys/js/梅花易数.js
+++ apps/mhys/js/梅花易数.js
@@ -1,11 +1,74 @@
 
-function 梅花易数(农历Obj) {
-    this.农历Obj = 农历Obj;
+var mhysUtils = {};
+
+// 辰年十二月十七日申时
+mhysUtils.get梅花易数From简易农历Str = function(dateStr) {
+    var 年, 月, 日, 时;
     
-    this.年数 = 0;
-    this.月数 = 0;
-    this.日数 = 0;
-    this.时数 = 0;
+    var regexp = /(.+)年(.+)月(.+)日(.+)时/gi;
+    var result;
+    
+    while ((result = regexp.exec(dateStr)) !== null) {
+        年 = result[1];
+        月 = result[2];
+        日 = result[3];
+        时 = result[4];
+    }
+    
+    年 = 六十甲子纳音表Utils.十二地支.findIndex(element => element === 年) + 1;
+    
+    for (var propName in 农历Utils.月份大写map) {
+        if (农历Utils.月份大写map[propName] === 月 + "月") {
+            月 = parseInt(propName);
+            break;
+        }
+    }
+    
+    for (var propName in 农历Utils.日期大写map) {
+        if (农历Utils.日期大写map[propName] === 日) {
+            日 = parseInt(propName);
+            break;
+        }
+    }
+    
+    时 = 六十甲子纳音表Utils.十二地支.findIndex(element => element === 时) + 1;
+    
+    var 梅花易数Obj = new 梅花易数();
+    梅花易数Obj.年数 = 年;
+    梅花易数Obj.月数 = 月;
+    梅花易数Obj.日数 = 日;
+    梅花易数Obj.时数 = 时;
+    梅花易数Obj.算出梅花卦象();
+    
+    return 梅花易数Obj;
+};
+
+
+mhysUtils.get梅花易数From农历Obj = function(农历Obj) {
+    var 梅花易数Obj = new 梅花易数();
+    
+    梅花易数Obj.农历Obj = 农历Obj;
+    梅花易数Obj.年数 = 六十甲子纳音表Utils.十二地支.indexOf(农历Obj.四柱.年柱.substring(1)) + 1;
+    梅花易数Obj.月数 = 农历Obj.月;
+    梅花易数Obj.日数 = 农历Obj.日;
+    梅花易数Obj.时数 = 六十甲子纳音表Utils.十二地支.indexOf(农历Obj.四柱.时柱.substring(1)) + 1;
+    梅花易数Obj.算出梅花卦象();
+    
+    return 梅花易数Obj;
+};
+
+function 梅花易数() {
+    //this.农历Obj = 农历Obj;
+    
+//    this.年数 = 六十甲子纳音表Utils.十二地支.indexOf(this.农历Obj.四柱.年柱.substring(1)) + 1;
+//    this.月数 = this.农历Obj.月;
+//    this.日数 = this.农历Obj.日;
+//    this.时数 = 六十甲子纳音表Utils.十二地支.indexOf(this.农历Obj.四柱.时柱.substring(1)) + 1;
+    
+    this.年数 = -1;
+    this.月数 = -1;
+    this.日数 = -1;
+    this.时数 = -1;
     
     this.动爻 = -1;
     
@@ -27,18 +90,10 @@ function 梅花易数(农历Obj) {
         下卦: null
     };
     
-    this.算出梅花卦象();
-    
-
+    //this.算出梅花卦象();
 }
 
-
 梅花易数.prototype.算出梅花卦象 = function() {
-    this.年数 = 六十甲子纳音表Utils.十二地支.indexOf(this.农历Obj.四柱.年柱.substring(1)) + 1;
-    this.月数 = this.农历Obj.月;
-    this.日数 = this.农历Obj.日;
-    this.时数 = 六十甲子纳音表Utils.十二地支.indexOf(this.农历Obj.四柱.时柱.substring(1)) + 1;
-    
     var 总数 = this.年数 + this.月数 + this.日数 + this.时数;
     
     var 上卦数 = (this.年数 + this.月数 + this.日数) % 8;
